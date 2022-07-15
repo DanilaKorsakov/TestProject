dc48c24bf90122382c87017348aab096a0674ca9
DanilaKorsakov <kvashnindan@gmail.com>
2022-07-15 14:05:59 +0300

    first commit

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<template>
+
+</template>
+
+<script>
+    import {defineComponent} from 'vue'
+    export default defineComponent ( {
+        name: 'App',
+        data (){
+            return {
+
+            }
+        },
+        methods: {
+
+        },
+    })
+</script>
